90929158ecc42b601c9919ff237e4b33a9d4810a
NahuelArg <claudioarganaraz86@gmail.com>
2026-01-15 11:17:51 +0100

    fix: update CORS origin to use ALLOWED_ORIGINS environment variable

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:4200', // Permite cualquier origen en desarrollo
+    origin: process.env.ALLOWED_ORIGINS|| 'http://localhost:4200', // Permite cualquier origen en desarrollo
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
